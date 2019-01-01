2915642c5ac134feaf4502b0f6dcf63f203bd1b8
Alvin <alvinsmith.nz@gmail.com>
2019-01-01 20:58:09 +1300

    Sry, warm up is 0. This one should be first

---
+++ 1.js
@@ -0,0 +1,11 @@
+function escape(input) {
+    // tags stripping mechanism from ExtJS library
+    // Ext.util.Format.stripTags
+    var stripTagsRE = /<\/?[^>]+>/gi;
+    input = input.replace(stripTagsRE, '');
+
+    return '<article>' + input + '</article>';
+}        
+
+// <body onload=prompt(1) 
+// tricky but work , no need for '>' close
