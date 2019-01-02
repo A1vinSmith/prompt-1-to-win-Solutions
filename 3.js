529976a5adccd109a3196d4bd1d5b05d56328837
Alvin <alvinsmith.nz@gmail.com>
2019-01-02 15:12:38 +1300

    2 ways close HTML comment

---
+++ 3.js
@@ -0,0 +1,9 @@
+function escape(input) {
+    // filter potential comment end delimiters
+    input = input.replace(/->/g, '_');
+
+    // comment the input to avoid script execution
+    return '<!-- ' + input + ' -->';
+}        
+
+// --!><svg onload=prompt(1)
